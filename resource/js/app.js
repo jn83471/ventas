3bbf4c366876ae42da5e08fb5fc6996b8f47a3dd
javf <jn83471@gmail.com>
2020-12-20 03:08:32 +0100

    salve los cambios echos

--- resource/js/app.js
+++ resource/js/app.js
@@ -2,8 +2,8 @@ var user=new User();
 var imguser=(evt)=>{
 	user.archivo(evt,'img');
 }
-var principal=new index();
+var principal=new Principal();
 $(document).ready(()=>{
 	let Urlactual=window.location.pathname;
-	principal.linkprincipal(Urlactual);
+	principal.linkPrincipal(Urlactual);
 })
---
+++ resource/js/index.js
@@ -0,0 +1,5 @@
+class Principal{
+    linkPrincipal(link){
+        alert("ca");
+    }
+}
